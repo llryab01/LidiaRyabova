0c24b68c570afd3e63afdc558c66789381909f1d
llryab01 <lidiaryabova@gmail.com>
2024-01-02 18:13:01 +0100

    Add files via upload

---
+++ multiqc.sh
@@ -0,0 +1,5 @@
+multiqc  -n multiqc_report_rnaseq \
+/D:\shared_D_folder\PitPat_analysis\only samples\to_submit_folde_PitPat\QualtyCounrol\RawDataQC/*zip \
+/D:\shared_D_folder\PitPat_analysis\only samples\to_submit_folde_PitPat\QualtyCounrol\TrimedFilesQC/ *zip \
+/D:\shared_D_folder\PitPat_analysis\only samples\to_submit_folde_PitPat\data\StarAlignedSamples/ *Log.final
+
